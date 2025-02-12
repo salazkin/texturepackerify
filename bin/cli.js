3dd475a18dff88c64d159a1958c6381311be2bb0
Oleksii Salazkin <salazkin@gmail.com>
2025-02-12 22:39:46 +0200

    fix cjs config

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 
 const path = require('path');
-const { pack } = require('../index');
+const fs = require('fs');
+const { pack } = require('../src/texturepackerify');
 
 const cwd = process.cwd();
 
 (async function () {
     let config = {};
+
+    const packageJson = JSON.parse(fs.readFileSync(path.join(cwd, 'package.json'), 'utf-8'));
+    const isModule = packageJson.type === 'module';
+
     try {
-        const configFilePath = path.join(cwd, ".texturepackerify.js");
+        const configFilePath = path.join(cwd, `.texturepackerify.${isModule ? "cjs" : "js"}`);
         config = require(configFilePath);
     } catch (error) {
 
